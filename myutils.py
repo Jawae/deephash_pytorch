e6228fb17d07ac202da09118f59602f57d0499a1
HoaTruong <dienhoa.t@gmail.com>
2018-11-17 22:45:46 +0100

    update helper function

--- myutils.py
+++ myutils.py
@@ -5,5 +5,9 @@ def run_folder(path, Learner):
     for i in (path).iterdir():
         Learner.predict(open_image(i))
 
-def test():
-        print('test')
+def binary_code(Learner):
+    for i in range(len(Learner.callbacks[0].outputs)):
+        Learner.callbacks[0].outputs[i] = Learner.callbacks[0].outputs[i] > 0
+
+def similarity(ten1, ten2):
+    return (ten1 == ten2).sum().float()/len(ten1)
